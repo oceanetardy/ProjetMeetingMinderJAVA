f8c8554b748b9400b944cda7d0889751a1b82f17
Océane TARDY <tardy.oceane42@gmail.com>
2024-04-15 11:34:56 +0200

    [+] Init Project

---
+++ MeetingMinder/src/main/java/MeetingMinder/MeetingMinder/MeetingMinderApplication.java
@@ -0,0 +1,13 @@
+package MeetingMinder.MeetingMinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingMinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingMinderApplication.class, args);
+	}
+
+}
---
+++ MeetingMinder/src/test/java/MeetingMinder/MeetingMinder/MeetingMinderApplicationTests.java
@@ -0,0 +1,13 @@
+package MeetingMinder.MeetingMinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingMinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
